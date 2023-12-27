725cc3d0a974bb0d85fed46d36c91d58a3ea609c
Chain Inspector <123721655+l1l2i@users.noreply.github.com>
2023-12-27 14:51:06 +0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -1,48 +1,91 @@
 import requests
 from datetime import datetime
+import time
+import pandas as pd
+from sqlalchemy import create_engine
 
 # Function to get the order book for a given instrument
 def get_book(depth=10, instrument='ETH-PERPETUAL'):
-    response = requests.get(f"https://test.deribit.com/api/v2/public/get_order_book?depth={depth}&instrument_name={instrument}")
+    response = requests.get(f"https://deribit.com/api/v2/public/get_order_book?depth={depth}&instrument_name={instrument}")
     return response.json()
 
-# Function to get ETH futures data from Deribit
-def get_eth_futures():
-    url = "https://www.deribit.com/api/v2/public/get_instruments?currency=ETH&kind=future&expired=false"
-    response = requests.get(url)
+def get_futures(currency='ETH'):
+    # Corrected by adding 'f' to make it an f-string for variable interpolation
+    response = requests.get(f"https://www.deribit.com/api/v2/public/get_instruments?currency={currency}&kind=future&expired=false")
     if response.status_code != 200:
         raise Exception("Failed to retrieve data from Deribit")
     return response.json()
 
 # Function to get ETH index price
-def get_eth_index_price():
-    url = "https://www.deribit.com/api/v2/public/get_index?currency=ETH"
+def get_index_price(currency='ETH'):
+    url = f"https://www.deribit.com/api/v2/public/get_index?currency={currency}"
     response = requests.get(url)
     if response.status_code != 200:
-        raise Exception("Failed to retrieve ETH index price from Deribit")
-    return response.json()['result']['ETH']
+        raise Exception("Failed to retrieve index price from Deribit")
+    return response.json()['result'][currency]
 
-# Main function to process ETH futures
-def process_eth_futures():
-    futures_data = get_eth_futures()
-    index_price = get_eth_index_price()
+# Function to save data to PostgreSQL database
+def save_to_db(df, engine, table='futures_data'):
+    df.to_sql(table, engine, if_exists='append', index=False)
 
+# Main function to process ETH futures and save data
+def process_futures(engine):
+    futures_data = get_futures(currency='ETH')
+    index_price = get_index_price(currency='ETH')
+    records = []
     for future in futures_data['result']:
-        order_book = get_book(instrument=future['instrument_name'])
-        best_bid = order_book['result']['best_bid_price']
-        best_ask = order_book['result']['best_ask_price']
-        midpoint = (best_bid + best_ask) / 2
+        order_book = get_book(instrument=future['instrument_name'], depth=5)
+        spot_book = get_book(instrument='ETH_USDC')
+        best_bid_spot = spot_book['result']['best_bid_price']
+        best_ask_spot = spot_book['result']['best_ask_price']       
+        best_bid_fut = order_book['result']['best_bid_price']
+        best_ask_fut = order_book['result']['best_ask_price']
+        ask_sizes = sum([ask['size'] for ask in order_book['result']['asks'][:5]])
+        bid_sizes = sum([bid['size'] for bid in order_book['result']['bids'][:5]])
+        market_delta = ask_sizes - bid_sizes
 
-        expiration_timestamp = future['expiration_timestamp']/1000
+        open_interest = order_book['result']['open_interest']
+        volume_fut = order_book['result']['volume']
+        volume_spot = spot_book['result']['volume']
+
+        midpoint = (best_bid_fut + best_ask_fut) / 2
+        entry_carry = best_bid_fut - best_ask_spot
+        exit_carry = -best_ask_fut + best_bid_spot
+        expiration_timestamp = future['expiration_timestamp'] / 1000
         days_until_expiration = (expiration_timestamp - datetime.now().timestamp()) / (60 * 60 * 24)
 
         index_difference = midpoint - index_price
+        percentage_difference = ((midpoint - index_price) / index_price) * 100
+        
+        if days_until_expiration > 0:
+            annualized_percentage_diff = percentage_difference * (365 / days_until_expiration)
+        else:
+            annualized_percentage_diff = 0  # To handle contracts that might have already expired or error in data
+        current_timestamp = datetime.now()
+        records.append({
+            "timestamp": current_timestamp,
+            "future": future['instrument_name'],
+            "midpoint": midpoint,
+            "days_until_expiration": days_until_expiration,
+            "index_difference": index_difference,
+            "index_price": index_price,
+            "percentage_difference": percentage_difference,
+            "annualized_percentage_diff": annualized_percentage_diff
+        })
+        print(f"Future: {future['instrument_name']}", "Annualized", f"{annualized_percentage_diff:.2f}%")
+    df = pd.DataFrame(records)
+    save_to_db(df, engine)
+    
+db_params = "postgresql://postgres:example@postgres:5432/mydb"
 
-        print(f"Future: {future['instrument_name']}")
-        print(f"Midpoint: {midpoint}")
-        print(f"Days Until Expiration: {days_until_expiration}")
-        print(f"Index Difference: {index_difference}")
-        print("-" * 30)
+engine = create_engine(db_params)
 
-# Run the main function
-process_eth_futures()
+process_futures(engine)
+
+'''
+if __name__ == "__main__":
+    while True:
+        process_futures(engine, args.currency, args.spot, args.depth)
+        time.sleep(1)
+
+'''
