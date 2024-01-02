ca10a6a0b0975f48b011f5b2a4d4f44ab9da1d9d
Chain Inspector <123721655+l1l2i@users.noreply.github.com>
2024-01-02 17:09:02 +0300

    Update profit-calc.py

--- profit-calc.py
+++ profit-calc.py
@@ -18,10 +18,6 @@ Fut2_entry = 43859
 Fut2_volume = 2.0009576141727
 Fut2_usd = 87760
 
-Pair1_max_profit = Fut1_entry*Fut1_volume - Spot1_entry*Spot1_volume
-Pair2_max_profit = Fut2_entry*Fut2_volume - Spot2_entry*Spot2_volume
-
-
 # Function to get the order book for a given instrument
 def get_book(depth=10, instrument='ETH-PERPETUAL'):
     response = requests.get(f"https://deribit.com/api/v2/public/get_order_book?depth={depth}&instrument_name={instrument}")
@@ -44,30 +40,53 @@ best_ask_fut_2 = fut2['result']['best_ask_price']
 best_bid_fut_2 = fut2['result']['best_bid_price']
 
 
+Pair1_max_profit = Fut1_entry*Fut1_volume - Spot1_entry*Spot1_volume
+Pair2_max_profit = Fut2_entry*Fut2_volume - Spot2_entry*Spot2_volume
 
 PnL_fut_1 = (Fut1_usd/best_ask_fut_1-Fut1_usd/Fut1_entry)*best_ask_spot
 PnL_spot_1 = (best_bid_spot - Spot1_entry)*Spot1_volume
 
-Port_usd_1 = (Spot1_volume - (Fut1_usd/best_ask_fut_1-Fut1_usd/Fut1_entry))*best_bid_spot
+Port_usd_1 = (Spot1_volume + (Fut1_usd/best_ask_fut_1-Fut1_usd/Fut1_entry))*best_bid_spot
 PnL_1 = PnL_spot_1 + PnL_fut_1
-
+CryptoBalance_1 = Spot1_volume + (Fut1_usd/best_ask_fut_1-Fut1_usd/Fut1_entry)
 
 PnL_fut_2 = (Fut2_usd/best_ask_fut_2-Fut2_usd/Fut2_entry)*best_ask_spot
 PnL_spot_2 = (best_bid_spot - Spot2_entry)*Spot2_volume
 
-Port_usd_2 = (Spot2_volume - (Fut2_usd/best_ask_fut_2-Fut2_usd/Fut2_entry))*best_bid_spot
+Port_usd_2 = (Spot2_volume + (Fut2_usd/best_ask_fut_2-Fut2_usd/Fut2_entry))*best_bid_spot
 PnL_2 = PnL_spot_2 + PnL_fut_2
+CryptoBalance_2 = Spot2_volume + (Fut2_usd/best_ask_fut_2-Fut2_usd/Fut2_entry)
 
 Total_port_value_usd = Port_usd_1 + Port_usd_2
 Total_PnL = PnL_1 + PnL_2
+Total_BTC = CryptoBalance_1 + CryptoBalance_2
 
-print(f"Future: {future['instrument_name']}")
-print(f"Midpoint: {midpoint}")
-print(f"Days Until Expiration: {days_until_expiration}")
-print(f"Index Difference: {index_difference}")
+
+
+#4.5 + (Fut1_usd/best_ask_fut_1-Fut1_usd/Fut1_entry) + (Fut2_usd/best_ask_fut_2-Fut2_usd/Fut2_entry)
+#(4.5 + (Fut1_usd/best_ask_fut_1-Fut1_usd/Fut1_entry) + (Fut2_usd/best_ask_fut_2-Fut2_usd/Fut2_entry))*best_bid_spot
+
+print(f"PnL_fut_1: {PnL_fut_1}")
+print(f"PnL_spot_1: {PnL_spot_1}")
+print(f"Port_usd_1: {Port_usd_1}")
+print(f"PnL_1: {PnL_1}")
 print("-" * 30)
-
-
+print(f"PnL_fut_2: {PnL_fut_2}")
+print(f"PnL_spot_2: {PnL_spot_2}")
+print(f"Port_usd_2: {Port_usd_2}")
+print(f"PnL_2: {PnL_2}")
+print("-" * 30)
+print(f"Total_port_value_usd: {Total_port_value_usd}")
+print(f"Total_PnL: {Total_PnL}")
+print(f"Pair1_max_profit: {Pair1_max_profit}")
+print(f"Pair2_max_profit: {Pair2_max_profit}")
+print("-" * 30)
+print(f"Total_port_value_usd: {Total_port_value_usd}")
+print(f"Total_PnL: {Total_PnL}")
+print("-" * 30)
+print(f"Total_BTC: {Total_BTC}")
+print(f"CryptoBalance_1: {CryptoBalance_1}")
+print(f"CryptoBalance_2: {CryptoBalance_2}")
 
 #market_profit = 
 #liquidate_profit
